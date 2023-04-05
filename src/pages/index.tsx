7275c53a6c02810d8136d73825f986bd6197a03b
MauriceMiot <maurice.yggdrasil@gmail.com>
2023-04-05 08:08:57 -0400

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <FirstBanner />
         <SecondBanner />
 
-        <Services ref={services} />
+        <Services />
 
         <Products />
         <Portfolio />
